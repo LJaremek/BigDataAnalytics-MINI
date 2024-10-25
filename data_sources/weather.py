55dee23b796528ffd73796084a0cc381542318d6
Łukasz Jaremek <jaremek.lukasz@gmail.com>
2024-10-25 12:51:07 +0200

    implement weather endpoint

---
+++ data_sources/weather.py
@@ -0,0 +1,103 @@
+import json
+
+import requests
+
+
+DAILY_PARAMETERS = (
+    "temperature_2m_min",
+    "temperature_2m_max",
+    "rain_sum",
+    "sunshine_duration"
+)
+
+COCOA_LOCATIONS = {
+    "Ivory Coast": {
+        "regions": [
+            {
+                "name": "Soubre",
+                "latitude": 5.46,
+                "longitude": 6.36,
+                "weight": 0.4
+            },
+        ],
+        "weight": 0.4
+    }
+}
+
+
+def generate_forecast_url(
+        latitude: float,
+        longitude: float,
+        hourly_parameters: list[str],
+        start_date: str | None = None,
+        end_date: str | None = None,
+        ) -> str:
+
+    url = "https://api.open-meteo.com/v1/forecast?"
+    url += f"latitude={latitude}&longitude={longitude}"
+    url += f"&daily={','.join(hourly_parameters)}"
+
+    if start_date is not None:
+        url += f"&start_date={start_date}"
+    if end_date is not None:
+        url += f"&end_date={end_date}"
+
+    return url
+
+
+def get_cocoa_weather(date: str) -> dict[str, float]:
+    results = {
+        "temperature": [],
+        "rain": [],
+        "sun": []
+    }
+
+    for _, country in COCOA_LOCATIONS.items():
+        cw = country["weight"]  # country_weight
+        country_results = {
+            "temperature": [],
+            "rain": [],
+            "sun": []
+        }
+
+        for region in country["regions"]:
+            print(region)
+            rw = region["weight"]  # region_weight
+
+            latitude = region["latitude"]
+            longitude = region["longitude"]
+            url = generate_forecast_url(
+                latitude, longitude, DAILY_PARAMETERS, date, date
+                )
+            response = requests.get(url)
+            data = json.loads(response.text)["daily"]
+
+            min_temp = data["temperature_2m_min"][0]
+            max_temp = data["temperature_2m_max"][0]
+
+            country_results["temperature"].append((min_temp + max_temp)/2 * rw)
+            country_results["rain"].append(data["rain_sum"][0] * rw)
+            country_results["sun"].append(data["sunshine_duration"][0] * rw)
+
+        temp = country_results["temperature"]
+        results["temperature"].append(sum(temp)/len(temp)*cw)
+        rain = country_results["rain"]
+        results["rain"].append(sum(rain)/len(rain)*cw)
+        sun = country_results["sun"]
+        results["sun"].append(sum(sun)/len(sun)*cw)
+
+    temp = results["temperature"]
+    results["temperature"] = sum(temp)/len(temp)
+    rain = results["rain"]
+    results["rain"] = sum(rain)/len(rain)
+    sun = results["sun"]
+    results["sun"] = sum(sun)/len(sun)
+
+    return results
+
+
+if __name__ == "__main__":
+    start_date = "2024-10-15"
+    end_date = "2024-10-15"
+
+    print(get_cocoa_weather(start_date))
