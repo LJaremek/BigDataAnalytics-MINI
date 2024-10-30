116a8ca983f7aa5caa1129afb2c58afda2073201
Lukasz Jaremek <Jaremek.Lukasz@gmail.com>
2024-10-30 19:45:02 +0100

    add worldnewsapi container with kafka connected

--- src/services/kafka_producers/scraper_worldnewsapi.py
+++ src/services/kafka_producers/scraper_worldnewsapi.py
@@ -9,6 +9,7 @@ import requests
 from data_processing.scraping.worldnewsapi_news import (
     worldnewsapi_generate_url, worldnewsapi_parse_news
 )
+from tools import get_kafka_producer
 
 
 if __name__ == "__main__":
@@ -17,31 +18,30 @@ if __name__ == "__main__":
     api_key = os.getenv("WORLDNEWSAPI_API_KEY")
     key_word = "cocoa"
 
-    producer = KafkaProducer(
-        bootstrap_servers=["kafka:9092"],
-        value_serializer=lambda v: json.dumps(v).encode("utf-8")
-    )
+    producer = get_kafka_producer()
 
     while True:
         start_date = "2024-10-15"  # TODO: automatyczna data
         end_date = "2024-10-18"  # TODO: automatyczna data
 
-        url = worldnewsapi_generate_url(
-            key_word, api_key,
-            start_date, end_date,
-            5.46, 6.36, 100
-            )
+        # url = worldnewsapi_generate_url(
+        #     key_word, api_key,
+        #     start_date, end_date,
+        #     5.46, 6.36, 100
+        #     )
 
-        response = requests.get(url)
-        articles = json.loads(response.text)["news"]
-        parsed_articles = worldnewsapi_parse_news(articles)
+        # response = requests.get(url)
+        # articles = json.loads(response.text)["news"]
+        # parsed_articles = worldnewsapi_parse_news(articles)
+        parsed_articles = {"data": "SOME INTERESTING", "date": "tomorrow"}
 
         data = {
-            "source": "scraper_news_xtb",
+            "source": "scraper_news_worldnewsapi",
             "news": parsed_articles,
             "time": time.strftime("%Y-%d-%m %I:%M:%S")
         }
 
         producer.send("scraped_data", value=data)
 
-        time.sleep(15*60)
+        # time.sleep(15*60)
+        time.sleep(10)
